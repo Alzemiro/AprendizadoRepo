9bdc3bcab8228f740ca8270579bb732b4e5551ca
Alzemiro Iago <iagomoraest@gmail.com>
2018-09-18 21:22:57 -0300

    Testando commit via terminal no GitLab

---
+++ Java/src/ControleDeFluxo1.java
@@ -0,0 +1,6 @@
+public class ControleDeFluxo1 {
+    public static void main(String[] args) {
+        if(true)
+            System.out.println("Se tiver apenas uma instrução de código não precisa usar as {}");
+    }
+}
